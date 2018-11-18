413a52cc6307f50b5de77e30e0129d357966e5ef
Desmond <>
2018-11-18 16:00:32 +0800

    using config file to retrieve directory path

--- ThisPython/crawler.py
+++ ThisPython/crawler.py
@@ -1,25 +1,27 @@
 from watchdog.observers import Observer
-from fileeventhandler import FileEventHandler
+#from fileeventhandler import FileEventHandler
 # from fileeventhandler import FileEventHandler
 from multiprocessing import Pool
-#from FileEventHandler import FileEventHandler
+from FileEventHandler import FileEventHandler
 import os
 import time
 import sys
 
 class Crawler(object):
-    def __init__(self):
-        self.path = r"F:\ThisPython\ThisPython\scandoc"
+    def __init__(self, path):
+        self.path = path
         self.event_handler = FileEventHandler(patterns=["*"])
         self.observer = Observer()
         self.observer.schedule(self.event_handler, path=self.path, recursive=True)
 
     # Walk through each files in a directory
+    @staticmethod
     def walk_dir(self):
-        print("Make a quick scan and index files in the path \"" + self.path + "\"")
-        for dirpath, dirs, files in os.walk(self.path, topdown=True):
-            for filename in files:
-                self.event_handler.scan_through(os.path.abspath(os.path.join(dirpath, filename)))
+        #print("Make a quick scan and index files in the path \"" + self.path + "\"")
+        #for dirpath, dirs, files in os.walk(self.path, topdown=True):
+        #    for filename in files:
+        #        os.path.abspath(os.path.join(dirpath, filename))
+        pass
 
     # Call watchdog in main thread only. 
     # If call in seperate thread/process,
--- ThisPython/run.py
+++ ThisPython/run.py
@@ -5,41 +5,58 @@ import sys
 if sys.version_info[0] == 3:
     pass
 elif sys.version_info[0] == 2:
-    sys.exit("Sorry, requires Python 3.x, not Python 2.x\n")
+    sys.exit(" * Sorry, requires Python 3.x, not Python 2.x\n")
 else:
-    sys.exit("Unknown Python version")
-
+    sys.exit(" * Unknown Python version")
 
 import argparse
 import os
 import threading
 from crawler import Crawler
 from frontend import flaskThread
+import json
+import os.path
+
+def load_json_config():
+    file = 'config.json'
+    key = 'monitor_path'
+    if (not os.path.exists(file)) or (os.path.getsize(file) <= 0):
+        # create a default config
+        config = {key: ""}
+        with open(file, 'w') as f:
+            json.dump(config, f)
+        sys.exit(" * Please setup the configuration file \"" + file +"\" to specify the monitor path")
+    else:
+        with open(file, 'r') as f:
+            config = json.load(f, strict=False)
+        if (key not in config) or (len(config[key]) == 0) or (config[key] == None):
+            sys.exit(" * Configuration file \"" + file + "\" -> " + key +": No target in given data")
+        else:
+            pass
+    return config
 
 if __name__ == "__main__":
-    #parser = argparse.ArgumentParser(description='Description of your program')
-    #parser.add_argument('-d','--directory', help='Description for directory argument', required=True)
-    #args = parser.parse_args()
-    #scan_dir = args.d
-
-    #if not os.path.exists(path):
-    #    sys.exit("The system cannot find the path: \"" + path + "\"")
-
-    #if not os.path.isdir(path):
-    #    sys.exit("Expecting folder/directory path to index the files, but a file is given as the argument")
-
+    config = load_json_config()
+    path = config["monitor_path"]
+    if not os.path.exists(path):
+        sys.exit(" * The system cannot find the path: \"" + path + "\"")
+    elif not os.path.isdir(path):
+        sys.exit(" * Expecting folder/directory path to index the files, but a file is given as the argument")
+    else:
+        pass
     
+    #threading.Thread(target=Crawler.walk_dir).start()
     threading.Thread(target=flaskThread).start()
 
     #path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "scandoc")
 
     try:
-        crawler = Crawler()
+        crawler = Crawler(path)
         crawler.start_watchdog()
-        print("Crawler and ElasticSearch components are ready")
+        print(" * Crawler and ElasticSearch components are ready")
     except ValueError as e:
         print(str(e))
-        sys.exit("System total failure, quit")
-    print("System is ready")
+        sys.exit(" * System total failure, quit")
+    print(" * System is ready")
 
     #crawler.walk_dir()
